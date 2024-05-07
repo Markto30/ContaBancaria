c7d30a2f89f81a88ed3f7f0a7ee361ad8d07c0ee
MarcosLuis <marcos27srs@gmail.com>
2024-05-07 06:36:05 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp2/ContaBancaria.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class ContaBancaria
+    {
+        public int nConta { get; private set; }
+        public string titularConta { get; set; }
+        public double saldoConta { get; private set; }
+
+        public ContaBancaria(int nConta, string titularConta)
+        {
+            this.nConta = nConta;
+            this.titularConta = titularConta;
+        }
+
+        public ContaBancaria(int nConta, string titularConta, double saldoConta) : this(nConta, titularConta)
+        {
+            this.saldoConta = saldoConta;
+        }
+
+        public override string ToString()
+        {
+            return $"Conta: {nConta}, Titular: {titularConta}, Saldo: ${saldoConta.ToString("f2", CultureInfo.InvariantCulture)}";
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaBancaria conta;
+
+            Console.Write("Entre com o número da conta: ");
+            int numero = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Entre com o nome do titular da conta: ");
+            string titular = Console.ReadLine();
+
+            Console.Write("Haverá depósito inicial (s/n)?: ");
+            char resposta = char.Parse(Console.ReadLine());
+
+            if (resposta == 's' || resposta  == 'S')
+            {
+                Console.Write("Entre com o valor do depósito inicial: $");
+                double depositoInicial = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                conta = new ContaBancaria(numero, titular, depositoInicial);
+            }
+            else
+            {
+                conta = new ContaBancaria(numero, titular);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Dados da Conta: ");
+            Console.WriteLine(conta);
+        }
+    }
+}
